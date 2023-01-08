fcf24b6563136cb0a840ba186660cf3b002fcf04
Shishir Biyyala <shishir.biyyala@invitae.com>
2023-01-08 17:28:33 -0500

    chore: dabbling with the basics...

--- src/clojure_playground/core.clj
+++ src/clojure_playground/core.clj
@@ -1,8 +1,10 @@
 (ns clojure-playground.core
   (:gen-class))
 
-(defn hello []
-  (println "Hello world!"))
+(defn hello [arg]
+  (println "Hello world," arg))
 
-(defn -main []
-  (hello))
+(defn -main
+  "I don't do a lot ... yet"
+  [& args]
+  (hello (first args)))
