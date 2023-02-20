317d329744f90c1b0d384eb708ba0066e7cd078a
shishir biyyala <shishir.biyyala@invitae.com>
2023-02-19 21:36:17 -0500

    * src/clojure_playground/cider_worksheet.clj:

--- src/clojure_playground/cider_worksheet.clj
+++ src/clojure_playground/cider_worksheet.clj
@@ -3,7 +3,37 @@
 
 (defn add [x] (+ x 100))
 
-(defn greet [name] (str "Hello," name))
+(defn greet [name] (str "Hello, " name))
 
 (greet "Shishir")
 
+(defn hello
+  ([]
+   (println "Debugging statements...")
+   "Hello Shishir")
+  ([name]
+   (str "Hello, " name)))
+
+(hello "Tendulkar")
+
+(hello)
+
+(println hello)
+
+(str 1)
+
+(def drinks ["Coke" "Fanta"])
+
+(defn can-drink? [age]
+  (let [a "Common Drink"]
+  (if (>= age 18)
+    (conj drinks "Beer" a)
+    (conj drinks "Cider" a))))
+
+drinks
+
+(can-drink? 17)
+
+(can-drink? 19)
+
+drinks
