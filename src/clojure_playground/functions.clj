9a68a087c5a19afba3f682ddefd27361a8759026
Shishir Biyyala <shishir.biyyala@invitae.com>
2023-02-20 21:23:50 -0500

    * src/clojure_playground/functions.clj:

--- src/clojure_playground/functions.clj
+++ src/clojure_playground/functions.clj
@@ -10,14 +10,6 @@
 
 #(println "Hello" %)
 
-;defs are evaluated only once whereas defns (with or without arguments) are evaluated (executed) every time they are
-; called. So if your functions always return the same value, you can change them to defs but not otherwise.
-(def increment (fn [x] (+ x 1)))
-
-(defn increment_set
-  [x]
-  (map increment x))
-
 (defn DataTypes []
   (def a 1)
   (def b 1.25)
@@ -34,7 +26,56 @@
   (println e)
   (println f)
   (println g)
-  (println h)
+  (println h))
 
-  )
 (DataTypes)
+
+(def status true)
+(def STATUS false)
+
+(println status)
+(println STATUS)
+
+(rem 7 6)
+
+
+(not= 6 7)
+
+(bit-and 0101 1100)
+
+(bit-or 0101 1100)
+
+(bit-xor 0101 1100)
+
+(bit-not 0101)
+
+(-(/ (+ 2 5) 3) 1)
+
+[1 23 43 43]
+
+'(1 2 3 344)
+
+(quote (1 2 3 344))
+
+(list 123 23432 4 23434 534)
+
+(cons 1 '(2 3 4 5))
+
+(first (list 1 2 4 5))
+
+(last (list 1 2 4 5))
+
+(count (list 1 2 4 5))
+
+(reduce + [1 34 434])
+
+;defs are evaluated only once whereas defns (with or without arguments) are evaluated (executed) every time they are
+;called. So if your functions always return the same value, you can change them to defs but not otherwise.
+(def increment (fn [x] (+ x 10)))
+
+(defn increment_set
+  [x]
+  (map increment x))
+
+(map increment [10 100 132])
+(increment_set [1 2 3])
