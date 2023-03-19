e7f2904794a1a6486bf52011c66cce797906f981
Shishir Biyyala <shishir.biyyala@invitae.com>
2023-03-18 23:05:51 -0400

    * src/clojure_playground/udemy_material.clj: added a coding problem

--- src/clojure_playground/udemy_material.clj
+++ src/clojure_playground/udemy_material.clj
@@ -183,3 +183,49 @@
 
 (ex-handling 1)
 (ex-handling "shishir")
+
+;; coding challenge
+
+(def dealership {'bmw 60000})
+
+(:bmw dealership)
+
+(defn is-code-valid
+  [code]
+  (defstruct coupon :Name :Discount)
+  (def valid-coupon (struct coupon "20Percent" 0.8))
+  (if (= (:Name valid-coupon) code)
+  true
+  false
+  ))
+
+(is-code-valid "20Percent")
+
+(defn get-car-prices
+  [budget code]
+  (def cars {"bmw" 60000, "ferrari" 10000, "fiat" 20000})
+  (if (is-code-valid code)
+    (do
+      (println "The code is valid")
+      (def discount (:Discount valid-coupon))
+      (doseq [car cars]
+        (def car-type (first car))
+        (def price (last car))
+        (def price-discount (* price discount))
+        (if (<= price-discount budget)
+          (println "The " car-type " costs " price-discount))
+        )
+      )
+    (do
+      (println "The code is invalid")
+      (doseq [car cars]
+        (def car-type (first car))
+        (def price (last car))
+        (if (<= price budget)
+          (println "The " car-type " costs " price))
+        )
+      )
+    )
+  )
+
+(get-car-prices 50000 "20Percents")
