18dce35b850c3ebdd6db47959acb09f5043254d2
Shishir Biyyala <shishir.biyyala@invitae.com>
2023-03-18 22:33:50 -0400

    * src/clojure_playground/udemy_material.clj:

--- src/clojure_playground/udemy_material.clj
+++ src/clojure_playground/udemy_material.clj
@@ -169,3 +169,17 @@
   )
 
 (destruct)
+
+;; Exception handling
+
+(defn ex-handling
+  [x]
+  (try
+    (inc x)
+    (catch ClassCastException e (println "Caught Exception:" (.getMessage e)))
+    (catch Exception e (println "Caught generic exception"))
+    (finally (println "cleanup and move on")))
+  )
+
+(ex-handling 1)
+(ex-handling "shishir")
